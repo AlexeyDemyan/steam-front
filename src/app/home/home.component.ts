4871be2e92e8f489dd8fec89728ea79d2e2c39e0
Alexey Demyanov <deal110988@him-mail.ch>
2025-03-21 16:38:40 +0100

    added filtering

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housing-location/housinglocation.type';
 import { HousingService } from '../services/housing.service';
-import { DetailsComponent } from '../details/details.component';
 
 @Component({
   selector: 'app-home',
@@ -11,13 +10,19 @@ import { DetailsComponent } from '../details/details.component';
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter />
+        <button
+          class="primary"
+          type="button"
+          (click)="filterResults(filter.value)"
+        >
+          Search
+        </button>
       </form>
     </section>
     <section class="results">
       <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
+        *ngFor="let housingLocation of filteredLocationList"
         [housingLocation]="housingLocation"
       ></app-housing-location>
     </section>
@@ -27,8 +32,22 @@ import { DetailsComponent } from '../details/details.component';
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    console.log(text);
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter((location) =>
+      location?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
