de6aa89264986e5eed5e4a3c6e82780d71b96717
Alexey Demyanov <deal110988@him-mail.ch>
2025-03-24 15:00:24 +0100

    testing out a very cool nav menu

--- src/app/appids-list/appids-list.component.ts
+++ src/app/appids-list/appids-list.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { AppIdItem } from '../appid-item/appid-item.component';
 import { AppId } from '../appid-item/appid-item.type';
 import { AppIdsService } from '../services/appids.service';
+import { NavMenuComponent } from '../nav-menu/nav-menu.component';
 
 @Component({
   selector: 'app-appids-list',
-  imports: [CommonModule, AppIdItem],
+  imports: [CommonModule, AppIdItem, NavMenuComponent],
   template: `
-    <h2 class="appids-list">Rendering AppIds list here!</h2>
+    <app-nav-menu />
     <app-appid-item *ngFor="let appid of appIdsList" [appId]="appid" />
   `,
   styleUrls: ['appids-list.component.css'],
