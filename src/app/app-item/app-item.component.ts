c0555b136331a3cae3954960781e9a377b456a69
Alexey Demyanov <deal110988@him-mail.ch>
2025-03-26 09:56:23 +0100

    added details/summary tag, commented out extra code from the snippet

--- src/app/app-item/app-item.component.ts
+++ src/app/app-item/app-item.component.ts
@@ -4,43 +4,49 @@ import { Component } from '@angular/core';
   selector: 'app-app-item',
   imports: [],
   template: `
-    <div class="app-item-box">
-      <div class="app-item-container">
-        <img
-          class="app-item-image"
-          src="/assets/desert.jpg"
-          alt="desert"
-          width="324"
-          height="150"
-        />
-        <h3 class="app-item-title">App Title</h3>
-        <div class="app-item-description">
-          <p class="app-item-description-text">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam,
-            culpa quos. Beatae libero excepturi nihil et fugit minus est debitis
-            id numquam aut! Eum facilis error illo? Harum, debitis voluptatum.
-          </p>
+    <details>
+      <summary>
+        <div class="app-item-box">
+          <div class="app-item-container">
+            <img
+              class="app-item-image"
+              src="/assets/desert.jpg"
+              alt="desert"
+              width="324"
+              height="150"
+            />
+            <h3 class="app-item-title">App Title</h3>
+            <div class="app-item-description">
+              <p class="app-item-description-text">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Numquam, culpa quos. Beatae libero excepturi nihil et fugit
+                minus est debitis id numquam aut! Eum facilis error illo? Harum,
+                debitis voluptatum.
+              </p>
+            </div>
+            <div class="app-item-tags">
+              <p>#Tag 1</p>
+              <p>#Another tag</p>
+              <p>#And yet another tag</p>
+              <p>#Oh one more tag!</p>
+              <p>#Tag 1</p>
+              <p>#Another tag</p>
+              <p>#And yet another tag</p>
+              <p>#Oh one more tag!</p>
+              <p>#Tag 1</p>
+              <p>#Another tag</p>
+              <p>#And yet another tag</p>
+              <p>#Oh one more tag!</p>
+              <p>#Tag 1</p>
+              <p>#Another tag</p>
+              <p>#And yet another tag</p>
+              <p>#Oh one more tag!</p>
+            </div>
+          </div>
         </div>
-        <div class="app-item-tags">
-          <p>#Tag 1</p>
-          <p>#Another tag</p>
-          <p>#And yet another tag</p>
-          <p>#Oh one more tag!</p>
-          <p>#Tag 1</p>
-          <p>#Another tag</p>
-          <p>#And yet another tag</p>
-          <p>#Oh one more tag!</p>
-          <p>#Tag 1</p>
-          <p>#Another tag</p>
-          <p>#And yet another tag</p>
-          <p>#Oh one more tag!</p>
-          <p>#Tag 1</p>
-          <p>#Another tag</p>
-          <p>#And yet another tag</p>
-          <p>#Oh one more tag!</p>
-        </div>
-      </div>
-    </div>
+      </summary>
+      <h3>Some other stuff</h3>
+    </details>
   `,
   styleUrls: ['app-item.component.css'],
 })
